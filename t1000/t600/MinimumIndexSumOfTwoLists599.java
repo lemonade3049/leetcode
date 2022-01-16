8a3a29e6d57f7073fdf2420b555277b04491784b
liu.yc <59647613+lemonade3049@users.noreply.github.com>
2022-01-16 17:18:44 +0900

    1.0

---
+++ t1000/t600/MinimumIndexSumOfTwoLists599.java
@@ -0,0 +1,53 @@
+package com.lemonade.leetcode.t1000.t600;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class MinimumIndexSumOfTwoLists599 {
+    class P {
+        String val;
+        int index;
+
+        P(String s, int i) {
+            val = s;
+            index = i;
+        }
+    }
+
+    public static void main(String[] args) {
+        new MinimumIndexSumOfTwoLists599().findRestaurant1(new String[]{"a", "b"}, new String[]{"b","a"});
+    }
+    public String[] findRestaurant1(String[] list1, String[] list2) {
+        HashMap<String, Integer> map1 = new HashMap<>();
+        PriorityQueue<P> p = new PriorityQueue<>((Comparator.comparingInt(o -> o.index)));
+
+        for (int i = 0; i < list1.length; i++) {
+            map1.put(list1[i], i);
+        }
+
+        for (int i = 0; i < list2.length; i++) {
+            if (map1.containsKey(list2[i])) {
+                p.offer(new P(list2[i], map1.get(list2[i]) + i));
+            }
+        }
+
+
+        int index = p.peek().index;
+        StringBuilder sb = new StringBuilder();
+
+        while (!p.isEmpty() && p.peek().index == index) {
+            P t = p.poll();
+            sb.append(t.val + "#");
+        }
+        String str = sb.toString();
+        str = str.substring(0, str.length() - 1);
+
+        return str.split("#");
+    }
+//
+//    public String[] findRestaurant(String[] list1, String[] list2) {
+//
+//        int[]
+//    }
+}
